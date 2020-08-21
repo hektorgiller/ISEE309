86a54ec6e32469e8a48d8ba1b08700301be102c7
hektorgiller <33809158+hektorgiller@users.noreply.github.com>
2020-08-21 13:58:08 +0300

    Add files via upload

---
+++ Models/ViewModels/PersonelFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PersonelMVC.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonelMVC.Models.ViewModels
+{
+    public class PersonelFormViewModel
+    {
+        public IEnumerable<Departman> departmanlar { get; set; }
+        public Personel Personel { get; set; }
+
+    }
+}
